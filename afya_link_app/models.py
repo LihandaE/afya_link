91c4c3747540d98c7bc1585e7325ec8f26369dcf
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-23 08:08:54 +0300

    Incooperation of doctor profile model.

--- afya_link_app/models.py
+++ afya_link_app/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
-import uuid
-from django.utils.timezone import now
 from django.conf import settings
 from django.utils import timezone
-from datetime import timedelta
 
-# Create your models here.
+
+
+# USER MANAGER
+
 
 class UserManager(BaseUserManager):
 
@@ -29,23 +28,19 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('role', 'super_admin')
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
+            raise ValueError("Superuser must have is_staff=True")
 
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+            raise ValueError("Superuser must have is_superuser=True")
 
         return self.create_user(email, password, **extra_fields)
 
 
-# Custom user model to support different roles and hospital association
+
+# CUSTOM USER MODEL (AUTH LAYER)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
-
-    specialities = models.ManyToManyField(
-        'Speciality',
-        blank=True,
-        related_name='doctors'
-    )
-
 
     ROLE_CHOICES = (
         ('super_admin', 'Super Admin'),
@@ -62,20 +57,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-
-    hospital = models.ForeignKey(
-        'Hospital',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='staff'
-    )
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
@@ -85,41 +70,106 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f"{self.email} - {self.role}"
-    
 
-class DoctorAvailability(models.Model):
-    doctor = models.ForeignKey(
+
+
+# HOSPITAL
+
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+# SPECIALITY
+
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+# DOCTOR PROFILE (PROFESSIONAL LAYER)
+
+
+class Doctor(models.Model):
+
+    doctor_profile = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         limit_choices_to={'role__in': ['doctor', 'consultant']},
+        related_name='doctor_profile'
+    )
+
+    specialities = models.ManyToManyField(
+        Speciality,
+        related_name='doctors'
+    )
+
+    hospitals = models.ManyToManyField(
+        Hospital,
+        related_name='doctors'
+    )
+
+    license_number = models.CharField(max_length=100, unique=True)
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    bio = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Dr. {self.doctor_profile.first_name} {self.doctor_profile.last_name}"
+
+
+
+# DOCTOR AVAILABILITY
+
+
+class DoctorAvailability(models.Model):
+
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
         related_name='availabilities'
     )
 
-    hospital = models.ForeignKey('Hospital', on_delete=models.CASCADE)
+    hospital = models.ForeignKey(
+        Hospital,
+        on_delete=models.CASCADE
+    )
 
-    day_of_week = models.CharField(max_length=20)  # Monday, Tuesday...
+    day_of_week = models.CharField(max_length=20)
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.doctor.email} - {self.day_of_week}"
+        return f"{self.doctor} - {self.day_of_week}"
     
 def is_doctor_available(doctor, hospital, date, time):
 
     day_name = date.strftime('%A')
 
-    availability = DoctorAvailability.objects.filter(
+    available = DoctorAvailability.objects.filter(
         doctor=doctor,
         hospital=hospital,
         day_of_week=day_name,
-        is_available=True,
         start_time__lte=time,
-        end_time__gte=time
+        end_time__gte=time,
+        is_available=True
     ).exists()
 
-    if not availability:
+    if not available:
         return False
 
     conflict = Appointment.objects.filter(
@@ -131,17 +181,9 @@ def is_doctor_available(doctor, hospital, date, time):
 
     return not conflict
 
-# Hospital model to represent healthcare facilities
-    
-class Hospital(models.Model):
-    name = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+# PATIENT
 
-# Patient model to represent individuals receiving healthcare services
 
 class Patient(models.Model):
     national_id = models.CharField(max_length=20, unique=True)
@@ -154,7 +196,11 @@ class Patient(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-# Visit model to represent a patient's visit to a hospital, linking patients and hospitals together    
+
+
+# VISIT
+
+
 class Visit(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='visits')
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
@@ -164,16 +210,11 @@ class Visit(models.Model):
     def __str__(self):
         return f"{self.patient} - {self.hospital}"
 
-# Speciality model to represent medical specialities that doctors can be associated with    
-class Speciality(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
 
 
-# Appointment model to represent scheduled appointments between patients and doctors at hospitals        
+# APPOINTMENT
+
+
 class Appointment(models.Model):
 
     STATUS_CHOICES = (
@@ -182,31 +223,31 @@ class Appointment(models.Model):
         ('cancelled', 'Cancelled'),
     )
 
-    patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
-    hospital = models.ForeignKey('Hospital', on_delete=models.CASCADE)
-
-    speciality = models.ForeignKey('Speciality', on_delete=models.SET_NULL, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    speciality = models.ForeignKey(Speciality, on_delete=models.SET_NULL, null=True)
 
     doctor = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        Doctor,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
-        limit_choices_to={'role__in': ['doctor', 'consultant']}
+        blank=True
     )
 
     appointment_date = models.DateField()
     appointment_time = models.TimeField()
 
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='scheduled')
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.patient} - {self.appointment_date}"
 
 
-# LabRecord, RadiologyRecord, Diagnosis, Prescription, AccessConsent, and AuditLog models to represent various aspects of a patient's medical record and access control mechanisms   
+
+# LAB RECORD
+
+
 class LabRecord(models.Model):
     visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name='lab_records')
     lab_tech = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -214,6 +255,11 @@ class LabRecord(models.Model):
     report_image = CloudinaryField('lab_report', folder='afyalink/labs')
     notes = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+# RADIOLOGY RECORD
+
 
 class RadiologyRecord(models.Model):
     visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name='radiology_records')
@@ -223,15 +269,24 @@ class RadiologyRecord(models.Model):
     notes = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
+
+# DIAGNOSIS
+
+
 class Diagnosis(models.Model):
     visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name='diagnoses')
-    doctor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
 
     patient_history = models.TextField()
     diagnosis = models.TextField()
     treatment_plan = models.TextField()
-
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+# PRESCRIPTION
+
 
 class Prescription(models.Model):
     visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name='prescriptions')
@@ -241,24 +296,31 @@ class Prescription(models.Model):
     dosage = models.CharField(max_length=100)
     duration = models.CharField(max_length=100)
     instructions = models.TextField()
-
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+# ACCESS CONSENT (OTP)
+
 
 class AccessConsent(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     requesting_hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    
+
     otp = models.CharField(max_length=6)
     is_verified = models.BooleanField(default=False)
     expires_at = models.DateTimeField()
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def is_expired(self):
         return timezone.now() > self.expires_at
-    
+
+
+# AUDIT LOG
+
+
 class AuditLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     action = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
+
