c0e022157db99c9580e89fd05c94e0e45f56cf8c
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-25 14:50:37 +0300

    set up conset app models.py

--- consent/models.py
+++ consent/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class AccessConsent(models.Model):
+    patient = models.ForeignKey("patients.Patient", on_delete=models.CASCADE)
+    requesting_hospital = models.ForeignKey("hospitals.Hospital", on_delete=models.CASCADE)
+
+    otp = models.CharField(max_length=6)
+    is_verified = models.BooleanField(default=False)
+    expires_at = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        return timezone.now() > self.expires_at
