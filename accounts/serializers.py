7c1cd95004f39efee9001bb4edb824f63dfaca12
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-25 15:16:43 +0300

    Set up serializers and views.py in accounts app

---
+++ accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields= [
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'role',
+            'hospital',
+        ]
+        read_only_fields=['id']
