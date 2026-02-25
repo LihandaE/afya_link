00135ac2ff7fad5f9246d8f170a468270f6841da
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-25 12:53:02 +0300

    Set up patients app models.py

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    national_id =models.CharField(max_length=50, unique=True)
+    first_name= models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    date_of_birth=models.DateField()
+    phone_number=models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
