e192e9fac01f9544db8fc5d636680e06df2c86dd
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-25 15:23:51 +0300

    Set up  serializers and views.py in hospital app

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Hospital
+from .serializers import HospitalSerializer
 
-# Create your views here.
+
+
+class HospitalViewSet(viewsets.ModelViewSet):
+    queryset = Hospital.objects.all()
+    serializer_class = HospitalSerializer
+    permission_classes = [IsAuthenticated]
+
+    
