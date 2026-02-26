ced4c8f4e13230251e76ed3a74576952bfd1d62e
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-02-26 16:10:17 +0300

    Set up urls in settings.py

--- afya_link_project/urls.py
+++ afya_link_project/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/v1/auth/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name= 'token_refresh'),
+
+    path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/hospitals/", include("hospitals.urls")),
+    path("api/v1/doctors/", include("doctors.urls")),
+    path("api/v1/patients/", include("patients.urls")),
+    path("api/v1/appointments/", include("appointments.urls")),
+    path("api/v1/records/", include("records.urls")),
+    path("api/v1/consent/", include("consent.urls")),
+
 ]
