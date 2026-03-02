e7616d3c2068a6a3e39358ecae556079f54e579f
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-02 11:28:42 +0300

    edit hospitals models.py

--- hospitals/models.py
+++ hospitals/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Hospital(models.Model):
     name = models.CharField(max_length=255, unique=True)
     location = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
