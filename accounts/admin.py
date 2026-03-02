7db608d341cb3e48f276afd0062b2bd217beb8fc
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-02 10:11:30 +0300

    Set up admin.py for accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display =['email',
+                   'first_name',
+                   'last_name',
+                   'role',
+                   'hospital',
+                   'is_staff',
+                   'is_active',
+                   ]
+    list_filter = [
+        'role',
+        'hospital',
+        'is_staff',
+        'is_active',
+    ]
+
+    search_fields =['email', 'fist_name', 'last_name'
+            ]
+    
+    fieldsets =(
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', { 'fields':('first_name', 'last_name', 'role', 'hospital')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')}),
+    
+    )
+
+    add_fieldsets= (
+        (None, {
+            "fields": ("email","first_name","last_name","role","hospital","password1","password2","is_staff","is_active",
+            ),
+        }),
+    ),
