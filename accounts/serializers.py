378d003c2e668d68faf4ce198ba442d67ebbe5de
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-03 17:02:42 +0300

    Edit serializers under accounts app

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,9 +9,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["is_staff", "is_superuser"]
 
-    def create(self, validated_data):
-        password = validated_data.pop("password")
-        user = User.objects.create(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
+def create(self, validated_data):
+    groups = validated_data.pop('groups', [])
+    password = validated_data.pop('password')
+
+    user = User.objects.create(**validated_data)
+    user.set_password(password)
+    user.save()
+
+    if groups:
+        user.groups.set(groups)
+
+    return user
--- patients/models.py
+++ patients/models.py
@@ -11,7 +11,7 @@ class PatientProfile(models.Model):
 
     national_id = models.CharField(max_length=20, unique=True)
     phone_number = models.CharField(max_length=20)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
