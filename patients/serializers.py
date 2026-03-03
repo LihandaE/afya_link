846b4fc3d7693ee2d120d8529d61a0086b6b0e17
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-03 17:28:24 +0300

    Edit views.py for patients app also its serializers.py to include patients profile

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-from accounts.models import  *
+from accounts.models import User
 from patients.models import PatientProfile
+
+
+# PATIENT REGISTRATION SERIALIZER- Used ONLY for creating patients
 
 
 class PatientRegistrationSerializer(serializers.ModelSerializer):
@@ -20,7 +23,9 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
             "phone_number",
             "date_of_birth",
         ]
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
 
     def create(self, validated_data):
 
@@ -28,11 +33,13 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
         phone_number = validated_data.pop("phone_number")
         date_of_birth = validated_data.pop("date_of_birth")
 
+        # Create user properly (with hashed password)
         user = User.objects.create_user(
             role="patient",
             **validated_data
         )
 
+        # Create patient profile
         PatientProfile.objects.create(
             user=user,
             national_id=national_id,
@@ -41,3 +48,26 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+
+# PATIENT PROFILE SERIALIZER -Used for listing and retrieving patients
+
+class PatientProfileSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(source="user.email", read_only=True)
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
+
+    class Meta:
+        model = PatientProfile
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "national_id",
+            "phone_number",
+            "date_of_birth",
+            "created_at",
+        ]
--- patients/views.py
+++ patients/views.py
@@ -1,22 +1,47 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
 from .models import PatientProfile
 from .serializers import *
 
 
 class PatientProfileViewSet(viewsets.ModelViewSet):
 
-    queryset = PatientProfile.objects.all()
-    serializer_class = PatientRegistrationSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
 
-        # Patient sees only their profile
+        # Patient sees only their own profile
         if user.role == "patient":
             return PatientProfile.objects.filter(user=user)
 
-        # Hospital staff see patients linked to their hospital visits
+        # Hospital staff / admins
         return PatientProfile.objects.all()
 
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return PatientRegistrationSerializer
+        return PatientProfileSerializer
+
+    
+    def perform_update(self, serializer):
+        user = self.request.user
+        instance = self.get_object()
+
+        # Prevent patient from editing someone else's record
+        if user.role == "patient" and instance.user != user:
+            raise PermissionDenied("You cannot modify another patient's profile.")
+
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+
+        # Patients should not delete profiles
+        if user.role == "patient":
+            raise PermissionDenied("Patients cannot delete their profile.")
+
+        instance.delete()
