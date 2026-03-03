964ff305f313de78e9d14bd717a1cc7e9f2ac82b
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-03 21:39:47 +0300

    Change in models.y in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -63,5 +63,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f"{self.email} ({self.role})"
-    
+        return f"{self.email} ({self.role})"  
