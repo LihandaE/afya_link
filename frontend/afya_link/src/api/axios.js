ecd901e9da324fff0972c052b2f76756d559fe00
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-16 11:09:30 +0300

    set up axios.js link with base url of backend

---
+++ frontend/afya_link/src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { getToken} from '../utils/tokenService';
+
+const API = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/v1',
+});
+
+API.interceptors.request.use((config) => {
+    const token= getToken();
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+export default API;
