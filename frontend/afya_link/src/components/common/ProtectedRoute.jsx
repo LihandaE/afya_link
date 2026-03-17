b8c612a529c03851636fe0d123e7c2fd497c9f36
Emmanuel Lihanda <emmanuelihanda@gmail.com>
2026-03-17 10:42:51 +0300

    Update ProtectedRoute

--- frontend/afya_link/src/components/common/ProtectedRoute.jsx
+++ frontend/afya_link/src/components/common/ProtectedRoute.jsx
@@ -1,8 +1,7 @@
-import { Navigate }  from "react-router-dom";
-import {getToken} from '../../utilis/tokenService';
+import { Navigate } from "react-router-dom";
+import { getToken } from "../../utils/tokenService";
 
-export default function ProtectedRoute({children}) {
-    const token = getToken();
-    return token ? children : <Navigate to ='/login' />;
-    
+export default function ProtectedRoute({ children }) {
+  const token = getToken();
+  return token ? children : <Navigate to="/login" replace />;
 }
--- frontend/afya_link/src/routes/AppRoutes.jsx
+++ frontend/afya_link/src/routes/AppRoutes.jsx
@@ -1,28 +1,22 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
 import ProtectedRoute from "../components/common/ProtectedRoute";
 
-export default function AppRoutes(){
-  return(
-    <BrowserRouter>
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
 
-      <Routes>
-
-        <Route path="/login" element={<LoginPage/>}/>
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardPage/>
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-  )
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
